ae40236568429bc8f936c0dfad3df4dce12993cd
Arpit Agrawal <147643369+arpit74170@users.noreply.github.com>
2025-11-30 13:03:09 +0530

    Add solution for Count Elements With at Least K Greater Values

--- LeetCode/Count-Elements-With-at-Least-K-Greater-Values/Solution.java
+++ LeetCode/Count-Elements-With-at-Least-K-Greater-Values/Solution.java
@@ -1,16 +1,21 @@
 1class Solution {
 2    public int countElements(int[] nums, int k) {
-3        int n = nums.length;
-4        if (k == 0) return n;  
-5
-6        Arrays.sort(nums);
-7        int kthLargest = nums[n - k];
-8
-9        int count = 0;
-10        for (int x : nums) {
-11            if (x < kthLargest) count++;
-12        }
-13        return count;
-14    }
-15}
-16
+3          if(k==0){
+4            return nums.length;
+5        }
+6        if(nums.length==1 ){
+7            return 0;
+8        }
+9      
+10        int n=nums.length;
+11        Arrays.sort(nums);
+12        int c=0;
+13        int kl=nums[n-k];
+14        for(int i=0;i<n-k;i++){
+15            if(nums[i]<kl){
+16                c++;
+17            }
+18        }
+19        return c;
+20    }
+21}
