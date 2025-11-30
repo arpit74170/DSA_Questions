517009652ee8cadcef1563697d56c13643923cb1
Arpit Agrawal <147643369+arpit74170@users.noreply.github.com>
2025-11-30 13:03:05 +0530

    Time: 41 ms (100%), Space: 134.1 MB (0%) - LeetHub

---
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countElements(int[] nums, int k) {
+        int n = nums.length;
+        if (k == 0) return n;  
+
+        Arrays.sort(nums);
+        int kthLargest = nums[n - k];
+
+        int count = 0;
+        for (int x : nums) {
+            if (x < kthLargest) count++;
+        }
+        return count;
+    }
+}
