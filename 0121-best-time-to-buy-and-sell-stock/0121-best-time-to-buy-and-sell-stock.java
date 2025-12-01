68d4c6fd4d65acf436de53385913e55e03b683f4
Arpit Agrawal <147643369+arpit74170@users.noreply.github.com>
2025-12-01 21:20:08 +0530

    Time: 1 ms (99.88%), Space: 94.3 MB (37.05%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,41 +1,18 @@
-// class Solution {
-//     public int maxProfit(int[] prices) {
-//         int cp=0;
-//         int p=0;
-//         int n=prices.length-1;
-//         int min=Integer.MAX_VALUE;
-//         for(int i=0;i<n;i++){
-//             if(prices[i]<min){
-//                 min=prices[i];
-//         }
-        
-//             cp=prices[i]-min;
-//             if(p<cp){
-//                 p=cp;
-            
-//             // p=Math.max(cp,p);
-        
-//         }
-//         }
-//         return p;
-//     }
-// }
 class Solution {
-    public int maxProfit(int[] prices)
-{
-        int lsf = Integer.MAX_VALUE; // least so far
-        int op = 0; // overall profit
-        int pist = 0; // profit if sold today
-        
-        for(int i = 0; i < prices.length; i++){
-            if(prices[i] < lsf){ // if we found new buy value which is more smaller then previous one
-                lsf = prices[i]; // update our least so far
+    public int maxProfit(int[] prices) {
+        int cp=0;
+        int p=0;
+        int n=prices.length-1;
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<=n;i++){
+            if(prices[i]<min){
+                min=prices[i];
             }
-            pist = prices[i] - lsf; // calculating profit if sold today by, Buy - sell
-            if(op < pist){ // if pist is more then our previous overall profit
-                op = pist; // update overall profit
+           cp=prices[i]-min;
+            if(p<cp){
+                p=cp;
             }
         }
-        return op; // return op 
-}
+        return p;
+    }
 }
