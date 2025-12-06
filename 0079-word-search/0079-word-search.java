24d437445fdcfff7bf0c1de45c4916daf2761eff
Arpit Agrawal <147643369+arpit74170@users.noreply.github.com>
2025-12-06 18:05:37 +0530

    Time: 205 ms (21.13%), Space: 42.9 MB (50.02%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -5,9 +5,7 @@ class Solution {
         if(i>=0 && j>=0 && i<grid.length && j<grid[0].length && grid[i][j]!='#' ){
             return true;
         }
-        else{
-            return false;
-        }
+        return false;
     }
     public boolean  solve(char grid[][],String word,int i,int j,int w){
         // if(word.length()==1 && grid[i][j]==word.charAt(w)){
@@ -52,48 +50,3 @@ class Solution {
     }
 }
 
-
-// class Solution {
-
-//     public boolean safe(char grid[][], int i, int j) {
-//         return i >= 0 && j >= 0 && i < grid.length && j < grid[0].length && grid[i][j] != '#';
-//     }
-
-//     public boolean solve(char grid[][], String word, int i, int j, int w) {
-//         // matched full word
-//         if (w == word.length()) {
-//             return true;
-//         }
-
-//         // out of bounds or char mismatch
-//         if (!safe(grid, i, j) || grid[i][j] != word.charAt(w)) {
-//             return false;
-//         }
-
-//         char temp = grid[i][j];
-//         grid[i][j] = '#'; // mark visited
-
-//         // explore 4 directions
-//         if (solve(grid, word, i + 1, j, w + 1)) return true;
-//         if (solve(grid, word, i - 1, j, w + 1)) return true;
-//         if (solve(grid, word, i, j + 1, w + 1)) return true;
-//         if (solve(grid, word, i, j - 1, w + 1)) return true;
-
-//         grid[i][j] = temp; // backtrack
-//         return false;
-//     }
-
-//     public boolean exist(char[][] grid, String word) {
-
-//         for (int i = 0; i < grid.length; i++) {
-//             for (int j = 0; j < grid[0].length; j++) {
-
-//                 if (grid[i][j] == word.charAt(0)) {
-//                     if (solve(grid, word, i, j, 0))
-//                         return true;
-//                 }
-//             }
-//         }
-//         return false;
-//     }
-// }
