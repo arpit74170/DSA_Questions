18493c7ed51f28271085193051f68dd8808d51ca
Arpit Agrawal <147643369+arpit74170@users.noreply.github.com>
2025-12-11 15:40:23 +0530

    Add solution for Duplicate Emails

--- LeetCode/Duplicate-Emails/Solution.sql
+++ LeetCode/Duplicate-Emails/Solution.sql
@@ -1,5 +1,2 @@
-# Write your MySQL query statement below
-SELECT email
-FROM Person
-GROUP BY email
-HAVING COUNT(email) > 1;
+1# Write your MySQL query statement below
+2select email from Person group by email  having count(email)>1;
