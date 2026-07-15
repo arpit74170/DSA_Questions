366a193962be897cc0283baefc7477debbefa27e
Arpit Agrawal <147643369+arpit74170@users.noreply.github.com>
2026-07-15 16:28:50 +0530

    Add solution for Longest String Chain

--- LeetCode/Longest-String-Chain/Solution.java
+++ LeetCode/Longest-String-Chain/Solution.java
@@ -1,49 +1,46 @@
 1class Solution {
-2
-3    public boolean isok(String s1,String s2){
-4        int i=0;
-5        int j=0;
-6        boolean skip=false;
-7        if(s1.length()+1 !=s2.length()){
-8            return false;
+2     HashMap<String,Integer> dp=new HashMap<>();
+3    public int longestStrChain(String[] words) {
+4       
+5
+6        int max = 1;
+7        for (int i = 0; i < words.length; i++) {
+8            max = Math.max(max, solve(words[i], words));
 9        }
-10            else{
-11              while(i !=s1.length() && j!=s2.length()){
-12                if(s1.charAt(i)!=s2.charAt(j)  ){
-13                    if(skip){
-14                        return false;
-15                    }
-16                    // j++;
-17                    j++;
-18                    skip=true;
-19                }
-20                else{
-21                    i++;j++;
-22                }
-23              }
-24            }
-25            return true;
-26    }
-27
-28    public int longestStrChain(String[] words) {
-29        Arrays.sort(words,(a,b)->a.length()-b.length());
-30        int max=1;
-31        int dp[]=new int[words.length];
-32        Arrays.fill(dp,1);
-33        for(int i=1;i<words.length;i++){
-34            for(int j=0;j<i;j++){
-35                if(isok(words[j],words[i])){
-36                    if(dp[i] < dp[j]+1 ){
-37                        dp[i]=dp[j]+1;
-38                        max=Math.max(max,dp[i]);
-39                    }
-40                }
+10        return max;
+11    }
+12
+13    private int solve(String curr, String[] words) {
+14        if(dp.containsKey(curr)){
+15            return dp.get(curr);
+16        }
+17        int best = 1;
+18        for (String next : words) {
+19            if (isPredecessor(curr, next)) {
+20                best = Math.max(best, 1 + solve(next, words));
+21            }
+22        }
+23        dp.put(curr,best);
+24        return best;
+25    }
+26
+27    private boolean isPredecessor(String s1, String s2) {
+28        if (s2.length() != s1.length() + 1) return false;
+29
+30        int i = 0, j = 0;
+31        boolean skipped = false;
+32
+33        while (i < s1.length() && j < s2.length()) {
+34            if (s1.charAt(i) == s2.charAt(j)) {
+35                i++;
+36                j++;
+37            } else {
+38                if (skipped) return false;
+39                skipped = true;
+40                j++;
 41            }
 42        }
-43        return max;
-44       
-45            
-46            
-47        
-48    }
-49}
+43        return true;
+44    }
+45}
+46
